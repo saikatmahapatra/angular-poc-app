94d093842ca1786d9bb5bf9701d4a7136797f67e
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2024-12-03 20:42:47 +0530

    mat button added

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
-
+const matUI = [
+  MatButtonModule
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    ...matUI
+  ],
+  exports: [matUI]
 })
 export class MaterialModule { }
